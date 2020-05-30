bb25af5ab89ae0e78f25ba0cb9bab4841dee53cc
Ivan Gamboa Ultreras <36210451+Zivangu9@users.noreply.github.com>
2020-05-30 13:22:59 -0500

    App - Visual - Agregado
    Se agrego la pantalla de registro

--- App/main.py
+++ App/main.py
@@ -2,11 +2,20 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 window = None
-login = Tk()
+register = None
+login = None
+
+
 def iniciarApp():
-	login.destroy()
+	global window, register, login
+	if not(register is None):
+		register.destroy()
+		register = None
+	if not(login is None):
+		login.destroy()
+		login = None
 	window = Tk()
-	window.geometry('600x700') 
+	centrar(window,600,700) 
 	window.resizable(False, False)
 	window.title("My Book Collection")
 	menuprincipal()
@@ -14,7 +23,12 @@ def iniciarApp():
 def menuprincipal():
 	pass	
 def showLogin():
-	login.geometry('400x400') 
+	global login, register
+	if not(register is None):
+		register.destroy()
+		register = None
+	login = Tk()
+	centrar(login,400,400) 
 	login.resizable(False, False)
 	login.title("Login")
 	usernameLabel = Label(login, text="User Name").grid(row=0, column=0)
@@ -27,7 +41,36 @@ def showLogin():
 		if (True):
 			iniciarApp()
 		pass
-	loginButton = Button(login, text="Login", command=validateLogin).grid(row=2, columnspan=2,sticky=W+E) 
+	registerButton = Button(login, text="Register", command=showRegister).grid(row=2, column=0,sticky=W+E) 
+	loginButton = Button(login, text="Login", command=validateLogin).grid(row=2, column=1,sticky=W+E) 
 	login.mainloop()
+def showRegister():
+	global login
+	global register
+	if not(login is None):
+		login.destroy()
+		login = None
+	register = Tk()
+	centrar(register,400,400)
+	register.resizable(False, False)
+	register.title("Registro")
+	usernameLabel = Label(register, text="User Name").grid(row=0, column=0)
+	username = StringVar()
+	usernameEntry = Entry(register, textvariable=username).grid(row=0, column=1)  
+	passwordLabel = Label(register,text="Password").grid(row=1, column=0)  
+	password = StringVar()
+	passwordEntry = Entry(register, textvariable=password, show='*').grid(row=1, column=1)
+	passwordLabel = Label(register,text="Repeat Password").grid(row=1, column=0)  
+	repassword = StringVar()
+	passwordEntry = Entry(register, textvariable=repassword, show='*').grid(row=1, column=1)
 
+	def validateRegister():
+		if (True):
+			iniciarApp()
+		pass
+	cancelButton = Button(register, text="Cancelar", command=showLogin).grid(row=2, column=0,sticky=W+E) 
+	registrarButton = Button(register, text="Register", command=validateRegister).grid(row=2, column=1,sticky=W+E) 
+	register.mainloop()
+def centrar(window,width, height):
+	window.geometry("{}x{}+{}+{}".format(width,height,int(window.winfo_screenwidth()/2 - width/2),int(window.winfo_screenheight()/2 - height/2)))
 showLogin()
